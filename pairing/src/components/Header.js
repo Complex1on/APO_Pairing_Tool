a23e6d07ac8be383210985f233193998b315bac8
Evan Yip <etyip@ucsb.edu>
2020-04-05 20:58:19 -0700

    implemented header check for login and access to all data

--- pairing/src/components/Header.js
+++ pairing/src/components/Header.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import GoogleButton from './GoogleButton';
 
-const Header = () => {
-    return (
-        <div className="ui secondary pointing menu">
-            <Link to="/" className="item">
-                Alpha Phi Omega Psi Pairing Tool
-            </Link>
-            <div className="right menu">
+class Header extends React.Component {
+    renderAllData() {
+        if (this.props.auth) {
+            return (
                 <Link to="/data/list" className="item">
                     All Data
                 </Link>
-                <GoogleButton />
+            );
+        } else {
+            return;
+        }
+    }
+    render() {
+        return (
+            <div className="ui secondary pointing menu">
+                <Link to="/" className="item">
+                    Alpha Phi Omega Psi Pairing Tool
+                </Link>
+                <div className="right menu">
+                    {this.renderAllData()}
+                    <GoogleButton />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        auth: state.auth,
+    };
 };
 
-export default Header;
+export default connect(mapStateToProps, {})(Header);
